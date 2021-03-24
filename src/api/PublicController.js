43042c3aa18288dd5229480946750ef4ffd0ae55
sunxiaojie79 <297280911@qq.com>
2021-03-25 00:34:41 +0800

    feat:邮件认证

--- src/api/PublicController.js
+++ src/api/PublicController.js
@@ -9,9 +9,8 @@ class PublicController {
       color: true,
       noise: Math.floor(Math.random() * 5),
       width: 150,
-      height: 50,
+      height: 38,
     })
-    console.log(newCaptca)
     ctx.body = {
       code: 200,
       data: newCaptca.data,
